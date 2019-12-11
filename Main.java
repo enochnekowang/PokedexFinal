816e2231b84c00b13ad7b41ea0814e3cc978e9ff
enochnekowang <58448136+enochnekowang@users.noreply.github.com>
2019-12-11 17:44:38 -0500

    1

--- Main.java
+++ Main.java
@@ -5,6 +5,14 @@ import java.lang.String;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bryant is awesome");
+            System.out.println("Bryant is awesome");
+            Pokedex MyPokedex = new Pokedex();
+            int userselection;
+
+            System.out.println("Please select a Pokemon ID number");
+            Scanner input = new Scanner;
+            userselection = input.nextInt();
+            MyPokedex.GetPokemon(userselection);
+
     }
 }
